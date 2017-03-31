b2fb2c8d5cabe7ff3cf9ab07a9dc72751803847b
Aleksandra Sikora <o.m.sikora@gmail.com>
2017-03-31 19:56:30 +0200

    Add UI bootstrap template

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,12 @@
-console.log('works');
+import angular from 'angular';
+import template from './app.html';
+import controller from './app.controller';
+import '../style/main.css';
+
+const AppComponent = {
+    template,
+    restrict: 'E',
+    controller
+};
+
+export default angular.module('app', []).component('app', AppComponent);
